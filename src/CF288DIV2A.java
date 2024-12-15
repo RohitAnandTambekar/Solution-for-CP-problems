5354d9ce726c7083710db764dbc38917fffcc293
RohitAnandTambekar <sairohittambekar@gmail.com>
2024-12-15 23:46:04 +0530

    -

---
+++ src/CF288DIV2A.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CF288DIV2A {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> bootset=new HashSet<Integer>();
+
+        for (String s:in.readLine().split(" ")){
+            bootset.add(Integer.parseInt(s));
+        }
+
+        System.out.println(4-bootset.size());
+    }
+}
